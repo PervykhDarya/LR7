ef88de330d0545d4aef4554166e187d791a4c33a
PervykhDarya <dashkapervykh@mail.ru>
2022-04-21 17:14:40 +0300

    добавлены примеры

---
+++ primer/primer5.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pathlib
+
+
+path = pathlib.PureWindowsPath(r'C:\Users\gahjelle\realpython\file.txt')
+print(path.name)
+print(path.parent)
+print(path.exists())
